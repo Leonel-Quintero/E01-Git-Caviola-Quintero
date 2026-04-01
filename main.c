8068f8b6b9c11251d524aed9e545afebaadae0a9
Leonel Quintero <leonelbenjaminquintero@gmail.com>
2026-04-01 02:02:17 -0300

    se modifico en rama 1

--- main.c
+++ main.c
@@ -4,4 +4,12 @@ void primero();
 void segundo();
 void iguales();
 
-int main();
+int main(){
+    int n1, n2;
+    printf("Ingrese el primer numero: ");
+    scanf("%d", &n1);
+    printf("Ingrese el segundo numero: ");
+    scanf("%d", &n2);
+    return 0;
+}
+;
